8e4893810a341c1dc39b86155842940bd29344a4
Aditi Bakshi <aditibakshi17@gmail.com>
2025-05-15 13:25:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { SortingVisualizer } from "./sortingVisualizer/SortingVisualizer.js"
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer />
+    </div>
+  );
+}
+
+export default App;
